f639300708a262881c0dc92cbe6ebd9d7d1f7b54
Lucas Constantino <lucasconstantino0102@gmail.com>
2025-01-09 15:24:58 -0300

    feat: [master] creating Database

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,12 +1,46 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  late Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute("CREATE TABLE $contactTable("
+          "$idColumn INTEGER PRIMARY KEY,"
+          "$nameColumn TEXT,"
+          "$emailColumn TEXT,"
+          "$phoneColumn TEXT,"
+          "$imgColumn TEXT)");
+    });
+  }
+}
 
 class Contact {
   int id = 0;
